fd6ccebab07fed19caceb9114c6d227b78efab0c
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-15 14:59:40 +0500

    fghv

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿//  CalculatR
 
 using System;
+using CalculatR.classes;
 
 class Programm
 {
     static void Main(string[] args)
     {
-
+        Securty securty = new Securty();
+        securty.checkedPassword();
     }
 }
---
+++ classes/Securty.cs
@@ -0,0 +1,19 @@
+namespace CalculatR.classes
+{
+    public class Securty
+    {
+        public void checkedPassword()
+        {
+            string Password = "";
+
+            do
+            {
+                System.Console.WriteLine("Enter Password");
+                Password = Console.ReadLine();
+                Console.Clear();
+            } while (Password != "0");
+
+
+        }
+    }
+}
